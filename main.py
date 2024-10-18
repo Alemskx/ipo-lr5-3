64069d865087abd5f1c92477784a59801f9ff2cd
Alemskx <alemskx@gmail.com>
2024-10-18 22:38:39 +0300

    задание 3

---
+++ main.py
@@ -0,0 +1,8 @@
+file = open('text.txt','r',encoding="utf-8")
+text = file.read()
+#Деление текста на слова
+words = text.split()
+words_count =0
+for i in words:
+    words_count+=1
+print("Количество слов:",words_count)
