58fd0419c9e525cdc852d00e1900c1f7b4048e5b
Alex <alemskx@gmail.com>
2024-10-21 00:11:25 +0300

    задание 3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-file = open('text.txt','r',encoding="utf-8")
-text = file.read()
+with open('text.txt','r',encoding="utf-8") as file:
+    text = file.read()
 #Деление текста на слова
 words = text.split()
 words_count =0
